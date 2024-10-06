48451447e01c2dc9fb2652c61b4f12d26e9b6fb4
Raja3120 <annareddyrajanandhanreddy@gmail.com>
2024-10-06 10:18:44 +0530

    Update

--- practice programs/program2.java
+++ practice programs/program2.java
@@ -1,6 +1,6 @@
 public static void main(String[] args) {
 		Student student1 = new Student();
-		Student student2 = new Student(101, "nithin", "comp-sc", 89.5f, (byte) 7, 9988554477L);
+		Student student2 = new Student(101, "Raja", "comp-sc", 89.5f, (byte) 7, 9988554477L);
 		
 		if (student1 == student2)
 			System.out.println("Both references refer to the same object");
